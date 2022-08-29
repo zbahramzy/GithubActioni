e3b597a8ae9676cf924da333aca63ae2a098aeb7
zbahramzy <zalandbahramzy@gmail.com>
2022-08-29 14:02:55 +0200

    Initial commit

---
+++ src/main/java/com/example/githubactioni/GithubActioniApplication.java
@@ -0,0 +1,13 @@
+package com.example.githubactioni;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubActioniApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GithubActioniApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/githubactioni/GithubActioniApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.githubactioni;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubActioniApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
